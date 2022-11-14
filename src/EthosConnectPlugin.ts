93ec77ba6c8a2f569f87977cd1686cced24fc267
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2022-11-14 15:31:48 -0600

    Rename main plugin

--- src/EthosConnectPlugin.ts
+++ src/EthosConnectPlugin.ts
@@ -3,10 +3,12 @@ import HelloWorld from './components/HelloWorld.vue'
 import Wrapper from './components/Wrapper.vue';
 
 // The Install function used by Vue to register the plugin
-export const PackagePlugin: Plugin = {
+const EthosConnectPlugin: Plugin = {
     install(app: App) {
         // register HelloWorld as a global component, so you can use it wherever you want in your app
         app.component('HelloWorld', HelloWorld)
         app.component('Wrapper', Wrapper)
     }
 }
+
+export default EthosConnectPlugin
