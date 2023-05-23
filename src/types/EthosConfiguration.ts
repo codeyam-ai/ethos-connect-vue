a4e1107e74ca5a4a21991b385bb45d9db5ac38f8
Jared Cosulich <jared.cosulich@gmail.com>
2023-05-23 07:13:17 -0400

    updating EthosConfiguration type

--- src/types/EthosConfiguration.ts
+++ src/types/EthosConfiguration.ts
@@ -1,9 +1,13 @@
 import { Chain } from '../enums/Chain'
 export interface EthosConfiguration {
-    apiKey?: string
-    walletAppUrl?: string
-    chain?: Chain
-    network?: string | number
-    hideEmailSignIn?: boolean
-    hideWalletSignIn?: boolean
+  apiKey?: string
+  walletAppUrl?: string
+  chain?: Chain
+  network?: string
+  hideEmailSignIn?: boolean
+  hideWalletSignIn?: boolean
+  preferredWallets?: string[]
+  redirectTo?: string;
+  disableAutoConnect?: boolean;
+  pollingInterval?: number;
 }
